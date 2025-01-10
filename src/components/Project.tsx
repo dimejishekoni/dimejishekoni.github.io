ad5cb6175c5387ea5b9d72d78bb6dc7bf498d4f2
Dimeji <44118919+dimejishekoni@users.noreply.github.com>
2025-01-10 22:47:05 +0000

    update to project page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import mock01 from '../assets/images/mock01.png';
-import mock02 from '../assets/images/mock02.png';
+import checkers from '../assets/images/checkers.png';
+import mitron from '../assets/images/mitron_bank.png';
+import bi360 from '../assets/images/bi360.png';
 import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/mock04.png';
 import mock05 from '../assets/images/mock05.png';
-import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
-import mock09 from '../assets/images/mock09.png';
 import mock10 from '../assets/images/mock10.png';
+import slr from '../assets/images/slr.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,14 +14,15 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://app.powerbi.com/view?r=eyJrIjoiMzkzNjkxMjgtNmI0Ny00ZDIyLWI1MTgtYmNiOTMyOGMyMzRiIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://app.powerbi.com/view?r=eyJrIjoiMzkzNjkxMjgtNmI0Ny00ZDIyLWI1MTgtYmNiOTMyOGMyMzRiIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9" target="_blank" rel="noreferrer"><img src={bi360} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://app.powerbi.com/view?r=eyJrIjoiMzkzNjkxMjgtNmI0Ny00ZDIyLWI1MTgtYmNiOTMyOGMyMzRiIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9" target="_blank" rel="noreferrer"><h2>Business Insight 360</h2></a>
                 <p>This is a comprehensive Power BI project that showcases dashboards for Finance, Sales, Supply Chain, Marketing, and Executive operations, providing actionable insights to optimize decision-making across various business sectors.</p>
             </div>
             <div className="project">
-                <a href="https://app.powerbi.com/view?r=eyJrIjoiYzQ4Mzg5ZTEtMzczNS00MzBmLTk3M2YtOTdiZjYyZTQ5ZTBlIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9&pageName=ReportSection3aac053726c8ce52444b" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://app.powerbi.com/view?r=eyJrIjoiYzQ4Mzg5ZTEtMzczNS00MzBmLTk3M2YtOTdiZjYyZTQ5ZTBlIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9&pageName=ReportSection3aac053726c8ce52444b" target="_blank" rel="noreferrer"><h2>Hotel Revenue & Market Insights Dashboard</h2></a>
+                <a href="https://app.powerbi.com/view?r=eyJrIjoiYzQ4Mzg5ZTEtMzczNS00MzBmLTk3M2YtOTdiZjYyZTQ5ZTBlIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9&pageName=ReportSection3aac053726c8ce52444b" target="_blank" rel="noreferrer"><img src={mitron} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://app.powerbi.com/view?r=eyJrIjoiYzQ4Mzg5ZTEtMzczNS00MzBmLTk3M2YtOTdiZjYyZTQ5ZTBlIiwidCI6ImM2ZTU0OWIzLTVmNDUtNDAzMi1hYWU5LWQ0MjQ0ZGM1YjJjNCJ9&pageName=ReportSection3aac053726c8ce52444b" target="_blank" rel="noreferrer"><h2>Strategic Insight For Mitron's Bank New Credit Card Line</h2></a>
                 <p>This is a Power BI project created to help AtliQ Grand, a leading hotel chain, regain market share and increase revenue through data-driven decisions. It delivers insights into key performance metrics, revenue distribution, and booking patterns, enabling better sales strategies and operational improvements.</p>
+                {/* <p>This is a Power BI project created to help AtliQ Grand, a leading hotel chain, regain market share and increase revenue through data-driven decisions. It delivers insights into key performance metrics, revenue distribution, and booking patterns, enabling better sales strategies and operational improvements.</p> */}
             </div>
             <div className="project">
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
@@ -41,11 +39,16 @@ function Project() {
                 <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><h2>Sales and Financial Analytics Report in Excel (FMCG)</h2></a>
                 <p>This is a project that leverages pivot tables, Power Query, and basic DAX functions to deliver actionable insights into customer discounts, negotiation strategies, and untapped markets. Designed to help businesses optimize sales efforts, it provides data-driven recommendations to enhance tactics, improve customer experiences, and boost revenue.</p>
             </div>
-            {/* <div className="project">
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
-            </div> */}
+            <div className="project">
+                <a href="https://github.com/dimejishekoni/Sign_To_Text" target="_blank" rel="noreferrer"><img src={slr} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/dimejishekoni/Sign_To_Text" target="_blank" rel="noreferrer"><h2>Sign Language Translation to Text</h2></a>
+                <p>Built an Advanced Sign Language Recognition (SLR) System developed using Google’s MediaPipe library for precise body pose landmark detection. This project involves a comparative study of LSTM and Transformer models, with results demonstrating the superior performance of the Transformer model in recognizing sign language gestures, highlighting its potential for real-time communication applications.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/dimejishekoni/Checkers" target="_blank" rel="noreferrer"><img src={checkers} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/dimejishekoni/Checkers" target="_blank" rel="noreferrer"><h2>AI-Powered Checkers Game</h2></a>
+                <p>Built with an intelligent AI powered checkers game using the Minimax algorithm. The game follows traditional 8x8 checkers rules, with key mechanics such as diagonal movement, forced captures, and piece promotion to kings upon reaching the opponent’s side. The Minimax algorithm, combined with heuristic evaluation, enables the AI to make optimal moves, providing a challenging and engaging gameplay experience.</p>
+            </div>
         </div>
     </div>
     );
